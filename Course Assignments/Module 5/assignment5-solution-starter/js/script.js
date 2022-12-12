7fb25a924542673e80cc55d297b2f0a7030e3e68
ervinjn <84634490+ervinjn@users.noreply.github.com>
2022-12-12 13:36:48 -0600

    Update script.js

--- Course Assignments/Module 5/assignment5-solution-starter/js/script.js
+++ Course Assignments/Module 5/assignment5-solution-starter/js/script.js
@@ -37,9 +37,10 @@ var menuItemHtml = "snippets/menu-item.html";
   // Given array of category objects, returns a random category object.
 var chooseRandomCategory = function (categories) {
   console.log("inside random category selector function");
-  console.log(categories.length);
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
+  console.log(randomArrayIndex);
+  console.log(categories[randomArrayIndex]);
 
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
